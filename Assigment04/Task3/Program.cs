96dac4dba197b6a1a0fe1744e561bcdd71ed75d0
Nino Kitiashvili <nkitiashvili@tbcbank.com.ge>
2023-06-16 18:06:17 +0400

    assigment04

---
+++ Assigment04/Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿//შესაქმნელია კონსოლური აპლიკაცია რომელიც მიიღებს ორი ერთგანზომილებიან მასივს, დალაგების მიმართლებას,
+//ზრდადებოთ (ASC) ან კლებადობით (DESC) გააერთიანებს მათ, დაალაგებს შესაბამისად და დაბეჭდავს შედეს.
+Console.WriteLine("masivis zoma:");
+int size = int.Parse(Console.ReadLine());
+int[] numb = new int[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"masivis elementi[{i}]");
+    int element = int.Parse(Console.ReadLine());
+    numb[i] = element;
+}
+Console.WriteLine("masivis zoma:");
+int size1 = int.Parse(Console.ReadLine());
+int[] numb1 = new int[size1];
+for (int i = 0; i < size1; i++)
+{
+    Console.WriteLine($"masivis elementi[{i}]");
+    int element1 = int.Parse(Console.ReadLine());
+    numb1[i] = element1;
+}
+int[] arr = new int[size + size1];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = numb[i];
+    }
+    for (int i = 0; i < size1; i++)
+    {
+        arr[size + i] = numb1[i];
+    }
+Console.WriteLine("rogor dalagdes zrdadobit Tu klebadobiT, zrdadobis shemtkhvevashi datseret ASC,klebadobis shemtkhvevashi DESC");
+string sort = Console.ReadLine();
+        if (sort == "ASC")
+    {
+        Array.Sort(arr);
+        Console.WriteLine("Ascending: ");
+        foreach (int value in arr)
+        {
+            Console.Write(value + " ");
+        }
+    }
+     else
+    {
+       Array.Reverse(arr);
+        Console.WriteLine("\n\nDescending: ");
+        foreach (int value in arr)
+        {
+            Console.Write(value + " ");
+        }
+    }
---
+++ Assigment04/Task5/Program.cs
@@ -0,0 +1,54 @@
+﻿//შესაქმნელია კონსოლური აპლიკაცია რომელსაც გადაეცემა ორი მატრიცია N x M და M x K ზომებით.
+//პირველი მატრიცის სვერების რაოდენობა უნდა უდრიდეს მეორე მატრიცის ხაზების რაოდენობას,
+//წინააღმდეგ შემთხვევაში გამოვიდეს შეტყობინება და მატრიცების შეყვანა მოითხოვოს თავიდან.
+//პროგრამამ უნდა გამოთვალოს მატრიცების ნამრავლი და დაბეჭდოს შედეგად მიღებული მატრიცა. N x M * M x K = N x K
+Console.WriteLine("pirveli matricis N zoma");
+int nmat1= int.Parse(Console.ReadLine());
+Console.WriteLine("pirveli matricis M zoma: ");
+int mmat1 = int.Parse(Console.ReadLine());
+int[,] mat = new int[nmat1, mmat1];
+for (int i = 0; i < nmat1; i++)
+{
+    for (int j = 0; j < mmat1; j++)
+    {
+        Console.WriteLine($"[{i}][{j}]");
+        int element = int.Parse(Console.ReadLine());
+        mat[i, j] = element;
+    }
+}
+Console.WriteLine("meore matricis M zoma");
+int mmat2 = int.Parse(Console.ReadLine());
+Console.WriteLine("meore matricis K zoma: ");
+int kmat2 = int.Parse(Console.ReadLine());
+int[,] mat2 = new int[mmat2, kmat2];
+for (int i = 0; i < mmat2; i++)
+{
+    for (int j = 0; j < kmat2; j++)
+    {
+        Console.WriteLine($"[{i}][{j}]");
+        int element2 = int.Parse(Console.ReadLine());
+        mat2[i, j] = element2;
+    }
+}
+Console.WriteLine("migebuli matrica aris:");
+int[,] matrix = new int[nmat1, kmat2];
+if (mmat1 == mmat2)
+{
+    int m, n, k;
+    for (m = 0; m < nmat1; m++)
+    {
+        for (n = 0; n < kmat2; n++)
+        {
+            matrix[m, n] = 0;
+            for (k = 0; k < mmat2; k++)
+                matrix[m, n] += mat[m, k] * mat2[k, n];
+            Console.Write(matrix[m, n] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+else {
+    Console.WriteLine("gamravleba ver mokherkhda");
+}
+    
+
