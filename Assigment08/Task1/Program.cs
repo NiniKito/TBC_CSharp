10bdb34c6cce1505b14b4edb1dca108e977c1e58
Nino Kitiashvili <nkitiashvili@tbcbank.com.ge>
2023-07-25 19:18:22 +0400

    assigment08

---
+++ Assigment08/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// დავალება #1შექმენით კონსოლური აპლიკაცია - სტატიკური კლასი კალკულატორი,რომელსაც ექნება მეთოდები 
+//Add - აბრუნებს გადაცემული ორი რიცხვის ჯამს;
+//Subtract - აბრუნებს გადაცემული ორი რიცხვის სხვაობას;
+//Multiply - აბრუნებს გადაცემული ორი რიცხვის ნამრალვს;
+//Divide - აბრუნებს გადაცემული ორი რიცხვის გაყოფის შედეგს;
+//Pow - აბრუნებს გადაცემული რიცხვის გადაცემულ ხარისხში აყვანის შედეგს;
+//Sqrt - აბრუნებს გადაცემული რიცხვის კვადრატულ ფესვს;
+//არ გამოიყენოთ Math კლასი.
+using Calculator;
+Console.WriteLine(Calculator.Calculator.Add(5, 8));
+Console.WriteLine(Calculator.Calculator.Subtract(13, 7));
+Console.WriteLine(Calculator.Calculator.Multiply(3, 6));
+Console.WriteLine(Calculator.Calculator.Divide(12, 4));
+Console.WriteLine(Calculator.Calculator.Pow(9, 2));
+Console.WriteLine(Calculator.Calculator.Sqrt(100));
